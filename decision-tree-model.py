a0b5bb24d5a175e6e73450aefed011278aa57146
Karol Ostrowski <karol@karol-macbook.local>
2025-02-26 15:52:27 +0100

    improving usability/logging

--- decision-tree-model.py
+++ decision-tree-model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import mlflow
+from sys import argv
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split, RandomizedSearchCV, StratifiedKFold
 from sklearn.metrics import accuracy_score
@@ -7,10 +8,18 @@ from scipy.stats import randint
 
 if __name__ == "__main__":
 
-    train_data = pd.read_csv("dataset_03_train.csv")
+    dataset_num = argv[1]
+    experiment_name = argv[2]
+    try:
+        run_name = argv[3]
+    except IndexError:
+        run_name = None
 
-    X = train_data.drop(columns=["Transported"]).values
-    y = train_data["Transported"].values
+    data = pd.read_csv(f"dataset_{dataset_num}_train.csv")
+    data_for_logging = mlflow.data.from_pandas(data, f"dataset_{dataset_num}_train.csv")
+
+    X = data.drop(columns=["Transported"]).values
+    y = data["Transported"].values
 
     X_train, X_test, y_train, y_test = train_test_split(X,
                                                         y,
@@ -18,7 +27,7 @@ if __name__ == "__main__":
                                                         random_state=9)
 
     mlflow.set_tracking_uri(uri="http://127.0.0.1:8080")
-    mlflow.set_experiment("spaceship-titanic-kaggle v2")
+    mlflow.set_experiment(experiment_name)
 
     model = DecisionTreeClassifier()
     param_distributions = {
@@ -31,23 +40,25 @@ if __name__ == "__main__":
     cv = StratifiedKFold(n_splits=5, shuffle=True)
     random_search = RandomizedSearchCV(estimator=model,
                                        param_distributions=param_distributions,
-                                       n_iter=1000,
+                                       n_iter=10,
                                        scoring="accuracy",
                                        cv=cv,
                                        verbose=1,
                                        n_jobs=-1)
 
-    random_search.fit(X_train, y_train)
+    with mlflow.start_run(run_name=run_name):
 
-    best_model = random_search.best_estimator_
-    best_params = random_search.best_params_
-    y_pred = best_model.predict(X_test)
-    test_acc = accuracy_score(y_test, y_pred)
-    print(f"Model accuracy: {test_acc*100:.2f}%")
+        random_search.fit(X_train, y_train)
 
-    with mlflow.start_run():
-        mlflow.set_tag("dataset", "03")
+        best_model = random_search.best_estimator_
+        best_params = random_search.best_params_
+        y_pred = best_model.predict(X_test)
+        test_acc = accuracy_score(y_test, y_pred)
+        print(f"Model accuracy: {test_acc*100:.2f}%")
+
+        mlflow.log_artifact(f"dataset_{dataset_num}_train.csv")
         mlflow.set_tag("model", "dt")
         mlflow.log_params(best_params)
         mlflow.log_metric("accuracy", test_acc)
-        mlflow.sklearn.log_model(best_model, "decision-tree")
+        mlflow.log_input(data_for_logging, "training")
+        mlflow.sklearn.log_model(best_model, "decision-tree", input_example=X_test[0])
